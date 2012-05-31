29e91072b60cccd128cdc8bbb66a78b9daf00f5b
Adam Nowak <adamus79@poczta.onet.pl>
2012-05-31 16:35:20 +0200

    Adding files to app/web.config transformation.

--- src/SmartWorking.Office/SmartWorking.Office.Services.Factory.IIS/ServiceFactoryIIS.cs
+++ src/SmartWorking.Office/SmartWorking.Office.Services.Factory.IIS/ServiceFactoryIIS.cs
@@ -13,21 +13,21 @@ namespace SmartWorking.Office.Services.Factory.IIS
 
     public IContractorsService GetContractorsService()
     {
-      var channelFactory = new ChannelFactory<IContractorsServiceChannel>();
+      var channelFactory = new ChannelFactory<IContractorsServiceChannel>("*");
       var service = channelFactory.CreateChannel();
       return service;
     }
 
     public IMaterialsService GetMaterialsService()
     {
-      var channelFactory = new ChannelFactory<IMaterialsServiceChannel>();
+      var channelFactory = new ChannelFactory<IMaterialsServiceChannel>("*");
       var service = channelFactory.CreateChannel();
       return service;
     }
 
     public IRecipesService GetRecipesService()
     {
-      var channelFactory = new ChannelFactory<IRecipesServiceChannel>();
+      var channelFactory = new ChannelFactory<IRecipesServiceChannel>("*");
       var service = channelFactory.CreateChannel();
       return service;
     }
