e8459c1d3be14b0757e083b79f6e4b2510c7e208
Adam Nowak <adamus79@poczta.onet.pl>
2012-06-25 00:13:01 +0400

    Update db scripts.

---
+++ db/deleteSampleData.sql
@@ -0,0 +1,18 @@
+DELETE FROM [Smartworking].[dbo].[DeliveryNotes]
+GO
+DELETE FROM [Smartworking].[dbo].[RecipeComponents]
+GO
+DELETE FROM [Smartworking].[dbo].[Buildings]
+GO
+DELETE FROM [Smartworking].[dbo].[Contractors]
+GO
+DELETE FROM [Smartworking].[dbo].[Cars]
+GO
+DELETE FROM [Smartworking].[dbo].[Drivers]
+GO
+DELETE FROM [Smartworking].[dbo].[Materials]
+GO
+DELETE FROM [Smartworking].[dbo].[MaterialStocks]
+GO
+DELETE FROM [Smartworking].[dbo].[Recipes]
+GO
